7f50239bd78874acf89882053aea40a4875881a5
A.H.M Towfique Mahmud <2005003@cse.buet.ac.bd>
2025-11-25 21:21:33 +0600

    CORS issue fixed

--- src/main/java/com/folio/repositories/UserRepository.java
+++ src/main/java/com/folio/repositories/UserRepository.java
@@ -2,8 +2,11 @@ package com.folio.repositories;
 
 import com.folio.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
